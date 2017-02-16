bd6a0f3c36350caf5b56d1cad56dc265161e29a6
Fatos Morina <fatosi.morina@gmail.com>
2017-02-16 07:28:11 +0100

    Set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :restaurants
-
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'restaurants#index'
 end
