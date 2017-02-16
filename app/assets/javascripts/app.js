51ed6a957ced10f0d7a8a244b4e1c7f375391bb0
Fatos Morina <fatosi.morina@gmail.com>
2017-02-16 10:47:53 +0100

    Implement Store

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,3 +2,17 @@ var Constans = {
   CHANGE_EVENT: 'change',
   ADD_COMMENT: 'comments.add'
 }
+
+var Store = new _.extend({}, EventEmitter.prototype, {
+  _comments: [],
+
+  addComment: function(comment){
+    this._comments[comment.id] = comment;
+  },
+
+  comments: function(){
+    this._comments;
+  }
+});
+
+
