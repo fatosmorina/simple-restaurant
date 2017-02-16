1970c2e49ea6da1d8eae04a517bf321844deaa4d
Fatos Morina <fatosi.morina@gmail.com>
2017-02-16 08:42:53 +0100

    Add key for each comment component

--- app/assets/javascripts/components/comment_list.js.jsx
+++ app/assets/javascripts/components/comment_list.js.jsx
@@ -3,7 +3,7 @@ class CommentList extends React.Component{
     return (
       <div>
       {JSON.parse(this.props.comments).map(function(comment){
-          return <Comment author={comment.author} body={comment.body} rank={comment.rank} />
+          return <Comment key={comment.id} author={comment.author} body={comment.body} rank={comment.rank} />
            }
         )
       }
